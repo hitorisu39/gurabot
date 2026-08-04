c54b7ddde2b56dac7f5eef4919686faf52031603
hitorisu39 <47534901+hitorisu39@users.noreply.github.com>
2026-08-04 22:56:28 +0300

    chore: add lint and prettier workflows

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,27 @@
 import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import { defineConfig } from "eslint/config";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import tseslint from "typescript-eslint";
 
 export default defineConfig(
     eslint.configs.recommended,
     tseslint.configs.recommended,
     {
-        ignores: ["dist/**", "node_modules/**", "src/core/**", "generated/**", "adapter/**", "scripts/**"],
+        ignores: [
+            "dist/**",
+            "node_modules/**",
+            "src/core/**",
+            "generated/**",
+            "adapter/**",
+            "scripts/**",
+        ],
     },
     {
         rules: {
-            "@typescript-eslint/ban-ts-comment": ["off"],
-            "@typescript-eslint/no-explicit-any": ["off"],
-            "@typescript-eslint/no-empty-object-type": ["off"],
+            "@typescript-eslint/ban-ts-comment": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-empty-object-type": "off",
         },
     },
-    eslintPluginPrettierRecommended,
+    eslintConfigPrettier,
 );
