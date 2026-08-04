2d216dd507c2b24201ec53ae69fca26de499d445
hitorisu39 <47534901+hitorisu39@users.noreply.github.com>
2026-08-04 20:05:00 +0300

    feat: add avatar command

---
+++ src/commands/osu/avatar/Avatar.command.ts
@@ -0,0 +1,25 @@
+import { Command, Import } from "@/core/decorators";
+import { CommandContext } from "@/core/discord/context/CommandContext";
+import { AbstractOsuCommand } from "../AbstractOsuCommand";
+import { ProfileViewService } from "@/modules/osu/profile/ProfileView.service";
+import { OsuService } from "@/modules/osu/Osu.service";
+import { ProfileFormatter } from "@domain/osu/formatters/Profile.formatter";
+
+@Command({
+    name: "avatar",
+    description: "Sends player's osu! profile picture.",
+    aliases: ["av"],
+})
+export class AvatarCommand extends AbstractOsuCommand {
+    @Import() declare private readonly profileViewService: ProfileViewService;
+    @Import() declare private readonly osuService: OsuService;
+
+    public async execute(ctx: CommandContext): Promise<void> {
+        const target = await this.resolveTarget(ctx);
+        const profile = await this.osuService.user(target.query, target.mode, target.server);
+        const embed = this.profileViewService.createBaseEmbed(profile, Date.now(), false);
+
+        embed.setThumbnail(null).setImage(ProfileFormatter.avatar(target.server, profile.id, Date.now()));
+        await ctx.respond(embed);
+    }
+}
