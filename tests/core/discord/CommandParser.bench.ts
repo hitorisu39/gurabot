162d530aeb0948797c15d6c9632d2234d0b62e34
hitorisu39 <47534901+hitorisu39@users.noreply.github.com>
2026-08-21 19:13:33 +0300

    chore: add tests for command parser

---
+++ tests/core/discord/CommandParser.bench.ts
@@ -0,0 +1,125 @@
+import "reflect-metadata";
+import { bench, describe } from "vitest";
+import { EInjectMode, EOptionType, IOptionMetadata } from "@/core/decorators";
+import { METAKEY_COMMAND_PROPERTIES } from "@/core/metakeys";
+import { CommandParser } from "@/core/discord/options/CommandParser";
+import { CommandContext } from "@/core/discord/context/CommandContext";
+
+function createContext(rawContent: string): CommandContext {
+    return {
+        isSlash: false,
+        rawContent,
+        state: {},
+    } as unknown as CommandContext;
+}
+
+function option(
+    data: Partial<IOptionMetadata> & Pick<IOptionMetadata, "propertyKey" | "name" | "type">,
+): IOptionMetadata {
+    return {
+        ...data,
+        description: data.description ?? "",
+        required: data.required ?? false,
+    };
+}
+
+class QueryDto {}
+
+Reflect.defineMetadata(
+    METAKEY_COMMAND_PROPERTIES,
+    [
+        option({
+            propertyKey: "cs",
+            name: "cs",
+            type: EOptionType.Range,
+        }),
+        option({
+            propertyKey: "ar",
+            name: "ar",
+            type: EOptionType.Range,
+        }),
+        option({
+            propertyKey: "od",
+            name: "od",
+            type: EOptionType.Range,
+        }),
+        option({
+            propertyKey: "length",
+            name: "length",
+            type: EOptionType.Range,
+        }),
+        option({
+            propertyKey: "creator",
+            name: "creator",
+            type: EOptionType.String,
+        }),
+    ],
+    QueryDto.prototype,
+);
+
+const simpleOptions = [
+    option({
+        propertyKey: "name",
+        name: "name",
+        type: EOptionType.String,
+        inject: EInjectMode.Greedy,
+    }),
+];
+
+const realisticOptions = [
+    option({
+        propertyKey: "name",
+        name: "name",
+        type: EOptionType.String,
+        inject: EInjectMode.Greedy,
+    }),
+
+    option({
+        propertyKey: "query",
+        name: "query",
+        type: EOptionType.Query,
+        queryDto: QueryDto,
+    }),
+
+    option({
+        propertyKey: "mods",
+        name: "mods",
+        type: EOptionType.Mods,
+    }),
+];
+
+const topIfOptions = [
+    option({
+        propertyKey: "name",
+        name: "name",
+        type: EOptionType.String,
+        inject: EInjectMode.Greedy,
+    }),
+
+    option({
+        propertyKey: "mods",
+        name: "mods",
+        type: EOptionType.ModsArray,
+    }),
+];
+
+describe("CommandParser", () => {
+    bench("simple username", async () => {
+        await CommandParser.parseAndValidate(createContext("mrekk"), simpleOptions);
+    });
+
+    bench("quoted username", async () => {
+        await CommandParser.parseAndValidate(createContext('"spaced username"'), simpleOptions);
+    });
+
+    bench("query example", async () => {
+        await CommandParser.parseAndValidate(
+            createContext("mrekk cs>=4 ar=10 od>=9.8 length>64 creator=sotarks +HD"),
+            realisticOptions,
+        );
+    });
+
+    bench("multiple mod operations", async () => {
+        await CommandParser.parseAndValidate(createContext("mrekk +HD -HR! +DT +FL -EZ!"), topIfOptions);
+    });
+});
